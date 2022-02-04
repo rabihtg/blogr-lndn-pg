fbd37fa8d2d3882dcf7ca79ec319012ee4764126
rabih harb <rabiharb.ss@outlook.com>
2022-02-04 21:40:53 +0200

    Finish Header

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const menuBtn = document.querySelector(".header__menu-btn");
 const headerNav = document.querySelector(".header__nav");
-const subMenuBtns = document.querySelectorAll(".header__sub-menu-items-cont");
+const subMenuBtns = document.querySelectorAll(".header__sub-menu-item");
 
 menuBtn.addEventListener("click", () => {
   headerNav.classList.toggle("off");
+  document.querySelector("body").classList.toggle("no-scroll");
   headerNav.classList.toggle("on");
 });
 
 subMenuBtns.forEach((btn) => {
-  console.log(btn);
-  btn.addEventListener("click", function () {
-    console.log(btn);
-    const subMenuItems = btn.querySelector(".header__sub-menu-items-cont");
+  btn.addEventListener("click", () => {
+    const subMenuItems = btn.nextElementSibling;
+    const arrow = btn.querySelector("picture");
+
+    btn.classList.toggle("on");
+    arrow.classList.toggle("rotate-x");
     subMenuItems.classList.toggle("off");
     subMenuItems.classList.toggle("on");
   });
