f5fd60cb9d7c0cb090842e0ee599c38c55906737
rabih harb <rabiharb.ss@outlook.com>
2022-02-06 13:46:00 +0200

    mobile design done, desktop almost

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
+"use strict";
 const menuBtn = document.querySelector(".header__menu-btn");
 const headerNav = document.querySelector(".header__nav");
 const subMenuBtns = document.querySelectorAll(".header__sub-menu-item");
+const body = document.querySelector("body");
 
 menuBtn.addEventListener("click", () => {
   headerNav.classList.toggle("off");
-  document.querySelector("body").classList.toggle("no-scroll");
   headerNav.classList.toggle("on");
+
+  if (headerNav.classList.contains("on")) {
+    menuBtn.src = "./images/icon-close.svg";
+  } else {
+    menuBtn.src = "./images/icon-hamburger.svg";
+  }
+
+  body.classList.toggle("no-scroll");
 });
 
 subMenuBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     const subMenuItems = btn.nextElementSibling;
     const arrow = btn.querySelector("picture");
-
     btn.classList.toggle("on");
     arrow.classList.toggle("rotate-x");
     subMenuItems.classList.toggle("off");
